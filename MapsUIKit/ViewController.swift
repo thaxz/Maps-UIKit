be6dacd228e75dedb473065d3143ff815173fc14
thaxz <thaxzt@gmail.com>
2023-08-24 19:54:16 -0300

    first commit

---
+++ MapsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsUIKit
+//
+//  Created by thaxz on 24/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
