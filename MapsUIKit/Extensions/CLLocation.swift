42f792221046c3c116b9de0ed23a5af9b949feda
thaxz <thaxzt@gmail.com>
2023-08-26 15:53:14 -0300

    feature(Develop): Extending CLLocation

---
+++ MapsUIKit/Extensions/CLLocation.swift
@@ -0,0 +1,18 @@
+//
+//  CLLocation.swift
+//  MapsUIKit
+//
+//  Created by thaxz on 26/08/23.
+//
+
+import Foundation
+import MapKit
+
+extension CLLocation {
+    
+    // apple park
+    static var `default`: CLLocation {
+        CLLocation(latitude: 36.063457, longitude: -95.880516)
+    }
+    
+}
