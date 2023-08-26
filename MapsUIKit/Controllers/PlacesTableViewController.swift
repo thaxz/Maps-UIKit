1f3d7e1f222c2bfb62caefbb007ad4d93d36c1a3
thaxz <thaxzt@gmail.com>
2023-08-26 16:13:13 -0300

    feature(Develop): Sheet now showing nearby places in a list format

--- MapsUIKit/Controllers/PlacesTableViewController.swift
+++ MapsUIKit/Controllers/PlacesTableViewController.swift
@@ -18,6 +18,23 @@ class PlacesTableViewController: UITableViewController {
         self.userLocation = userLocation
         self.places = places
         super.init(nibName: nil, bundle: nil)
+        // registering cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PlaceCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        places.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath)
+        let place = places[indexPath.row]
+        // cell configuration
+        var content = cell.defaultContentConfiguration()
+        content.text = place.name
+        content.secondaryText = "Coming"
+        cell.contentConfiguration = content
+        return cell
     }
     
     required init?(coder: NSCoder) {
